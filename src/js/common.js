a7a7d7d8624361583f74e13d9f211cca86a3cdae
Zhycho <avd.qu6@gmail.com>
2020-04-02 18:51:00 +0500

    Сделал страницу категории

--- src/js/common.js
+++ src/js/common.js
@@ -8,12 +8,30 @@ $(document).ready(function () {
         rows: 2,
         swipe: false,
         vertical: false,
-        // prevArrow: $('.js--special-products__list-nav-prev'),
-        // nextArrow: $('.js--special-products__list-nav-next'),
-        // customPaging : function(slider, i) {
-        //     var thumb = $(slider.$slides[i]).data('thumb');
-        //     return '<div class="special-products__list-nav-numbers-item">'+(i+1)+'</div>';
-        // },
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesPerRow: 3
+                }
+            },
+            {
+                breakpoint: 920,
+                settings: {
+                    slidesPerRow: 2
+                }
+            }
+        ]
+    });
+
+    $('.js--category-slider').slick({
+        arrows: false,
+        infinite: false,
+        dots: true,
+        slidesPerRow: 4,
+        rows: 3,
+        swipe: false,
+        vertical: false,
         responsive: [
             {
                 breakpoint: 1200,
